3dcea42d16b5da064de443881b3fe3ee9fbdfb47
Sebastian Kachniarz <seba1234767@gmail.com>
2026-06-29 18:36:28 +0200

    added controller and communication models

---
+++ Controllers/CardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZadanieProgramistyczneMillenium.Models;
+using ZadanieProgramistyczneMillenium.Services;
+
+namespace ZadanieProgramistyczneMillenium.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CardsController(ICardService cardService, ICardAllowedActionService allowedActionService) : ControllerBase
+{
+    [HttpPost("allowed-actions")]
+    public async Task<IActionResult> GetAllowedActions(GetAllowedActionsRequest request)
+    {
+        var cardDetails = await cardService.GetCardDetails(request.UserId, request.CardNumber);
+
+        if (cardDetails == null)
+        {
+            return NotFound($"Card with number '{request.CardNumber}' for user '{request.UserId}' was not found");
+        }
+
+        var allowedActions = allowedActionService.GetAllowedActions(cardDetails);
+
+        var response = new GetAllowedActionsResponse(
+            CardNumber: cardDetails.CardNumber,
+            AllowedActions: allowedActions
+        );
+
+        return Ok(response);
+    }
+}
---
+++ Models/CardDtoModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZadanieProgramistyczneMillenium.Models;
+
+public record GetAllowedActionsRequest(
+    [Required(AllowEmptyStrings = false, ErrorMessage = "UserId is required and cannot be empty")]
+    string UserId,
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "CardNumber is required and cannot be empty")]
+    string CardNumber
+);
+
+public record GetAllowedActionsResponse(
+    string CardNumber,
+    IEnumerable<string> AllowedActions
+);
