5f4431779e0bd09f58b9a361aa0c86cbe2f98423
Sebastian Kachniarz <seba1234767@gmail.com>
2026-06-29 17:10:53 +0200

    repo preparation - removed weather template and added required example code to use

---
+++ Models/CardModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ZadanieProgramistyczneMillenium.Models;
+
+public enum CardType
+{
+    Prepaid,
+    Debit,
+    Credit
+}
+public enum CardStatus
+{
+    Ordered,
+    Inactive,
+    Active,
+    Restricted,
+    Blocked,
+    Expired,
+    Closed
+}
+public record CardDetails(string CardNumber, CardType CardType, CardStatus CardStatus, bool IsPinSet);
